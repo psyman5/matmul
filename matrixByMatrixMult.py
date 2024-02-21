e3f9778389536704cd2418102605ebc62cffdae8
Ethan <88164097+psyman5@users.noreply.github.com>
2024-02-21 12:47:08 -0500

    Update matrixByMatrixMult.py

--- matrixByMatrixMult.py
+++ matrixByMatrixMult.py
@@ -1,14 +1,31 @@
+from dotProd import dotProd
+from matrixClass import matrix
 
 def multMatrix(m1, m2): #TODO: implement matrix multiplication
-    from matrixClass import matrix
+    
     if m1.rows != m2.columns:
         print("Incompatible Matrices!")
         return
     
     rowCounter = 0
     columnCounter = 0
-    newMat = matrix(rows=m1.rows,columns=m2.columns, elements= [], entries = "")
+    newMat = matrix(rows=m1.rows,columns=m2.columns, elements= [], 
+                    entries = [], columnizedElements= [])
+    #newMat.constructMatrix(newMat.elements,newMat.rows,newMat.columns, entries = "")
+    
 
-    for row in m1.rows:
-        for element in row:
-            dot = element * m2.elements[columnCounter]
+    
+    m2.columnizeMatrix(m2.elements, [], m2.columns, m2.rows)
+
+    for index, aRow in enumerate(m1.elements):
+        
+        for bRow in m2.columnizedElements:  
+            prod = dotProd(a = aRow, b = m2.columnizedElements[index])
+            print(index, prod)
+            
+            newMat.entries.append(prod)
+    
+    newMat.constructMatrix(newMat.elements, newMat.rows, newMat.columns, newMat.entries)
+    
+
+    return newMat
